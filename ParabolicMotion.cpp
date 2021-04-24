f5227c7c0e2168c4e873cb419c39b12ecf3b7a6a
Stefano <stefanobianchi314@gmail.com>
2021-04-24 16:17:25 +0200

    Added real motion. Also, the last version wasn't working, use this.

--- ParabolicMotion.cpp
+++ ParabolicMotion.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 #include<cmath>
 #include<TGraph.h>
-//#include<TH1.h>
+#include<TLegend.h>
 using namespace std;
 
-double Time = 10.;
-double h = 0.1;
+const double g = -9.81;
+const double Time = 10.;//a.u.
+const double h = 0.06;
+const double Theta = 30.*3.14/180.;
+
 int steps = (Time/h);
 double r0[2] = {0.,3.};//x0, y0
 double v0 = 20.;
@@ -21,16 +24,14 @@ struct SpacePoint {
 	SpacePoint(double x0, double y0, double v0, double theta){
 		x[0] = x0;
 		y[0] = y0;
-		vx[0] = v0*cos(theta*3.14/180.);
-		vy[0] = v0*sin(theta*3.14/180.);
+		vx[0] = v0*cos(theta);
+		vy[0] = v0*sin(theta);
 	}
 };
 
-SpacePoint Trajectory(double *r0, double v0, double theta, int steps, double inc){
-	SpacePoint point(steps, r0[0], r0[1], v0, theta);
-	
-	double g = -9.81;
-	for(i_pos=0; i_pos<steps; i_pos++){
+SpacePoint Trajectory(double *r0, double v0, double theta, int Steps, double inc){
+	SpacePoint point(r0[0], r0[1], v0, theta);
+	for(i_pos=0; i_pos<Steps; i_pos++){
 		point.x[i_pos+1] = point.x[i_pos]+inc*point.vx[0];
 		if(point.y[i_pos]<0.) break;
 		point.y[i_pos+1] = point.y[i_pos]+inc*point.vy[i_pos];
@@ -39,21 +40,32 @@ SpacePoint Trajectory(double *r0, double v0, double theta, int steps, double inc
 	}
 	return point;
 }
-
+   
 int main() {
-	SpacePoint plot = Trajectory(r0,v0,30.,steps,h);
+	SpacePoint plot = Trajectory(r0,v0,Theta,steps,h);
 	double *x = new double[i_pos-1];
 	double *y = new double[i_pos-1];
+	double *y_real = new double[i_pos-1];
+	cout<<i_pos<<endl;
 	for(int i=0; i<(i_pos-1); i++){
 		x[i]=plot.x[i];
 		y[i]=plot.y[i];
-		cout<<"X position: "<<x[i]<<endl;
-		cout<<"Y position: "<<y[i]<<endl<<endl;
+      	y_real[i] = r0[1] + v0*sin(Theta)*i*h+0.5*g*i*i*h*h;
 		}
+	TCanvas *c1=new TCanvas;
 	TGraph *gr = new TGraph(i_pos-1,x,y);
+	TGraph *real = new TGraph(i_pos-1,x,y_real);
 	gr->SetMarkerColor(kRed);
 	gr->SetMarkerSize(1);
 	gr->SetMarkerStyle(8);
-	gr->Draw("APE");
+      real->SetMarkerColor(kGreen);
+      real->SetMarkerSize(1);
+	real->SetMarkerStyle(8);
+	real->Draw("APE");
+	gr->Draw("p same");
+	auto legend = new TLegend();
+   	legend->AddEntry(real,"Real Motion");
+   	legend->AddEntry(gr,"Euler Integration");
+   	legend->Draw("same");
 }
 
